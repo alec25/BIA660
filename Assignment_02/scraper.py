f8ec21ef2abd7e0390d189483104f8e0be8f8dd6
Alec Kulakowski <alecthekulak@gmail.com>
2018-03-07 23:38:08 -0500

    Ok, question 1 done, 2a done, 2b done, 3a almost
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- Assignment_02/scraper.py
+++ Assignment_02/scraper.py
@@ -1,5 +1,5 @@
-# driver.find_elements_by_tag_name
-"""
+"""# driver.find_elements_by_tag_name
+
 BIA-660A: Assignment 02
 Alec Kulakowski
 I pledge my honor that I have abided by the Stevens honor system.
@@ -39,8 +39,6 @@ sleep(random()+0.5) # WAIT WAIT WAIT #stats_table
 sleep(random()) # WAIT WAIT WAIT
 # team_table = driver.find_element_by_id('datagrid').find_element_by_tag_name('tbody')
 team_table = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
-
-
 # sorted_team_data = [team.text for team in sorted_teams]
 # print(sorted_team_data)
 def get_table(table_path):
@@ -81,41 +79,71 @@ if(np.mean(al_homeruns) > np.mean(nl_homeruns)):
 else:
     answer_two_a_name = "NL"
     answer_two_a_avg = np.mean(nl_homeruns)
-# ok great 2a done
+# ok great 2a done ###################################################################################
 # inning_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_class_name('season_select')
 # Select(year_selector).select_by_value('2015')
 game_type_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_name('st_hitting_hitting_splits')
 game_type_selector.click()
 sleep(random()+0.5) # WAIT WAIT WAIT
 Select(game_type_selector).select_by_visible_text('First Inning')#.select_by_value('i01')
-team_table_ = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
-table_two_b = get_table(team_table)
-
-
-
-
-# print(eee)
-# for team in table_path.find_elements_by_tag_name('tr'):
-#     [eh.text for eh in team.find_elements_by_tag_name('td')]
-
-
-# data = pd.[[e.text for e in team.find_elements_by_tag_name('td')] for team in team_table.find_elements_by_tag_name('tr')]
+sleep(random()+0.1)
+team_table_2 = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
+table_two_b = get_table(team_table_2)
+table_two_b.sort_values(by='League', inplace=True)
+table_two_b.to_csv("Assignment_02/Question_2b.csv")
+al_homeruns_b = []
+nl_homeruns_b = []
+for team_data in table_two_b.iterrows():
+    if team_data[1]["League"]=="AL":
+        al_homeruns_b = al_homeruns_b + [int(team_data[1]["HR"])]
+    else:
+        nl_homeruns_b = nl_homeruns_b + [int(team_data[1]["HR"])]
+if(np.mean(al_homeruns_b) > np.mean(nl_homeruns_b)):
+    answer_two_b_name = "AL"
+    answer_two_b_avg = np.mean(al_homeruns_b)
+else:
+    answer_two_b_name = "NL"
+    answer_two_b_avg = np.mean(nl_homeruns_b)
+#ok whew, 2b done too #####################################################################################
+# stats_header_bar = driver.find_element_by_class_name('megamenu-static-navbar__menu-button-label')
+# stats_header_bar.click()
+# sleep(random())
+# stats_header_bar = driver.find_element_by_class_name('megamenu-navbar-overflow__menu-item--stats')
+# stats_header_bar.click()
+team_button = driver.find_element_by_id('content_wrapper').find_element_by_link_text("Player")
+team_button.click()
+sleep(random()) # WAIT WAIT WAIT
+year_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_class_name('season_select')
+Select(year_selector).select_by_value('2017')
+sleep(random()) # WAIT WAIT WAIT
+game_type_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_name('sp_hitting_game_type') #st?
+game_type_selector.click()
+sleep(random()+0.5) # WAIT WAIT WAIT
+Select(game_type_selector).select_by_visible_text('Regular Season')
+sleep(random()+0.5) # WAIT WAIT WAIT #stats_table
+team_selector = driver.find_element_by_id('sp_hitting-1').find_element_by_class_name('team_id')
+team_selector.click()
+sleep(random()+0.5) # WAIT WAIT WAIT
+Select(team_selector).select_by_visible_text('New York Yankees')
+sleep(random()+0.5) # WAIT WAIT WAIT
+game_type_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_name('sp_hitting_hitting_splits') #st
+game_type_selector.click()
+sleep(random()+0.5) # WAIT WAIT WAIT
+Select(game_type_selector).select_by_visible_text('Select Split')#.select_by_value('i01')
+sleep(random()+0.1)
+home_run_sorter = driver.find_element_by_id('datagrid').find_element_by_tag_name('thead').find_element_by_class_name('dg-ops')
+home_run_sorter.click() #this works but it's uncessesary
 #
-# [team for team in team_table.find_elements_by_tag_name('tr')]
-# team = team_table.find_elements_by_tag_name('tr')[0]
-# [''.join([eh.text for eh in team.find_elements_by_tag_name('td')]) for team in team_table.find_elements_by_tag_name('tr')]
-# driver.close()
+question_3_a_table = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
+q3_table = get_table(question_3_a_table)
+q3_table_min30 = q3_table.loc[[int(i)>=30 for i in q3_table["AB"]]]
 
 
 
 
 
-# stats_line_items = stats_header_bar.find_elements_by_tag_name('li')
-# sleep(random())
-# stats_line_items[0].click()
-# sleep(random())
-# hitting_season_element = driver.find_element_by_id('sp_hitting_season')
-# season_select = Select(hitting_season_element)
+
+
 
 
 # Last thing, close driver
@@ -123,3 +151,4 @@ table_two_b = get_table(team_table)
 driver.close()
 print('Question 1: The team is "{}"'.format(answer_one))
 print('Question 2a: The league is {} (avg home runs: {})'.format(answer_two_a_name, answer_two_a_avg))
+print('Question 2b: The league is {} (avg home runs: {})'.format(answer_two_b_name, answer_two_b_avg))
