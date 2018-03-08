680546bb3ebafc72295576880edd829b4110711d
Alec Kulakowski <alecthekulak@gmail.com>
2018-03-07 23:10:24 -0500

    Ok, question 1 done, 2a done
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- Assignment_02/scraper.py
+++ Assignment_02/scraper.py
@@ -39,7 +39,6 @@ sleep(random()+0.5) # WAIT WAIT WAIT #stats_table
 sleep(random()) # WAIT WAIT WAIT
 # team_table = driver.find_element_by_id('datagrid').find_element_by_tag_name('tbody')
 team_table = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
-# table_path = team_table #TODO: remove
 
 
 # sorted_team_data = [team.text for team in sorted_teams]
@@ -61,8 +60,38 @@ def get_table(table_path):
     #         element = table_path
 table_one = get_table(team_table)
 table_one.sort_values(by='HR', ascending=False, inplace=True)
-table_one.to_csv("Assignment_02/question01.csv")
+table_one.to_csv("Assignment_02/Question_1.csv")
 answer_one = table_one.iloc[0,:].loc["Team"]
+#ok, whew, done with that. 90% of the effort went into that one question,
+#but i guess it's only going to account for 1/5 of the grade for some reason...
+table_two = table_one.sort_values(by='League', inplace=False)
+table_two.to_csv("Assignment_02/Question_2a.csv")
+# [eh for eh in table_two if eh.loc["League"]=="AL"]
+# [print(eh) for eh in table_two.iterrows()]
+al_homeruns = []
+nl_homeruns = []
+for team_data in table_two.iterrows():
+    if team_data[1]["League"]=="AL":
+        al_homeruns = al_homeruns + [int(team_data[1]["HR"])]
+    else:
+        nl_homeruns = nl_homeruns + [int(team_data[1]["HR"])]
+if(np.mean(al_homeruns) > np.mean(nl_homeruns)):
+    answer_two_a_name = "AL"
+    answer_two_a_avg = np.mean(al_homeruns)
+else:
+    answer_two_a_name = "NL"
+    answer_two_a_avg = np.mean(nl_homeruns)
+# ok great 2a done
+# inning_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_class_name('season_select')
+# Select(year_selector).select_by_value('2015')
+game_type_selector = driver.find_element_by_id('widgets').find_element_by_class_name('onscreen').find_element_by_name('st_hitting_hitting_splits')
+game_type_selector.click()
+sleep(random()+0.5) # WAIT WAIT WAIT
+Select(game_type_selector).select_by_visible_text('First Inning')#.select_by_value('i01')
+team_table_ = driver.find_element_by_id('datagrid').find_element_by_tag_name('table')
+table_two_b = get_table(team_table)
+
+
 
 
 # print(eee)
@@ -91,5 +120,6 @@ answer_one = table_one.iloc[0,:].loc["Team"]
 
 # Last thing, close driver
 # driver.close()
-
-print('Question 1: The answer is "{}"'.format(answer_one))
+driver.close()
+print('Question 1: The team is "{}"'.format(answer_one))
+print('Question 2a: The league is {} (avg home runs: {})'.format(answer_two_a_name, answer_two_a_avg))
