6906989e2fbff6ce30dcdde4bfbc933e705d1544
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-11 15:28:22 -0400

    Assignment 03 started
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

---
+++ Assignment_03/review_scraper.py
@@ -0,0 +1,12 @@
+"""
+BIA-660A: Assignment 03
+Alec Kulakowski
+I pledge my honor that I have abided by the Stevens honor system.
+"""
+# Import Statements
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from random import random
+from time import sleep
+# import numpy as np
+import pandas as pd
