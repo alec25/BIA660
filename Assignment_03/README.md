230c07c462350e1b4cf16ab414b6f1a1711958df
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-11 15:51:48 -0400

    Assignment 03 started, readme added
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

---
+++ Assignment_03/README.md
@@ -0,0 +1,16 @@
+# Assignment 03
+
+Alec Kulakowski \
+I pledge my honor that I have abided by the Stevens Honor System 
+
+### Extra
+
+Conda environment included:\
+spacy nltk jupyter requests selenium beautifulsoup4 tensorflow keras
+
+Also ran:\
+pip install msgpack-python==0.5.4\
+python -m spacy download en\
+pip install pycorenlp\
+conda install scikit-learn
+
--- Assignment_03/review_scraper.py
+++ Assignment_03/review_scraper.py
@@ -10,3 +10,8 @@ from random import random
 from time import sleep
 # import numpy as np
 import pandas as pd
+# Accessing the URL
+url = "https://www.amazon.com/RockBirds-Flashlights-Bright-Aluminum-Flashlight/product-reviews/B00X61AJYM"
+driver = webdriver.Chrome(executable_path="chromedriver.exe")# "Assignment_03/chromedriver.exe"
+driver.get(url=url)
+sleep(1.5) # WAIT WAIT WAIT
