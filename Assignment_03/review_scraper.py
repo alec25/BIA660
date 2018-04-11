74c6b674ab8b886efcb7562701c8c3bc73109372
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-11 16:19:03 -0400

    Assignment 03 update
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- Assignment_03/review_scraper.py
+++ Assignment_03/review_scraper.py
@@ -15,3 +15,10 @@ url = "https://www.amazon.com/RockBirds-Flashlights-Bright-Aluminum-Flashlight/p
 driver = webdriver.Chrome(executable_path="chromedriver.exe")# "Assignment_03/chromedriver.exe"
 driver.get(url=url)
 sleep(1.5) # WAIT WAIT WAIT
+# Apply filters and sort
+filter_section = driver.find_element_by_id("cm_cr-view_opt_sort_filter")
+reviewer_filter = filter_section.find_element_by_id("a-autoid-12") #-announce
+reviewer_filter.click()
+sleep(random())
+Select(reviewer_filter).select_by_visible_text('Verified purchase only')
+sleep(random())
