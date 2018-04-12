de8ec6228df6f44db7c80ed701865f4feb7b5da6
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-11 20:51:27 -0400

    Assignment 03; review_scraper version one, troubleshooting
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- Assignment_03/review_scraper.py
+++ Assignment_03/review_scraper.py
@@ -30,7 +30,7 @@ rated_filter.click()
 sleep(0.05 + random())
 rated_filter = driver.find_element_by_id("sort-order-dropdown")
 Select(rated_filter).select_by_value("recent")
-sleep(0.05 + random())
+sleep(0.15 + random())
 # Harvest Reviews
 # review_section = driver.find_element_by_id("cm_cr-review_list").find_elements_by_class_name("review") #"a-section review"
 df = pd.DataFrame(columns=["stars", "title", "author", "date", "body", "style", "helpful"])
@@ -38,18 +38,19 @@ day_limit = datetime(2017, 1, 1).toordinal()
 done = False
 page_num = 1
 review_count = 0
-while (done==False & page_num <= 4):
+while (done==False): #& page_num <= 4
     print("page num: " + str(page_num))
 
     review_section = driver.find_element_by_id("cm_cr-review_list").find_elements_by_class_name("review")  # "a-section review"
+    sleep(0.1 + random())
     for review in review_section:
         review_count += 1
-        # stars = int(review.find_element_by_class_name("a-link-normal").get_attribute("title")[0]) # first char is the #stars
+        stars = int(review.find_element_by_class_name("a-link-normal").get_attribute("title")[0]) # first char is the #stars
         # stars = review.find_element_by_css_selector("i[data-hook='review-star-rating'").find_element_by_class_name("a-icon-alt").text[0]
-        stars = review.find_element_by_css_selector("a[class='a-link-normal'").get_attribute("title")[0]
+        # stars = review.find_element_by_css_selector("a[class='a-link-normal'").get_attribute("title")[0]
         title = review.find_element_by_class_name("review-title").text
-        author = review.find_element_by_css_selector("a[data-hook='review-author'").text
-        # author = review.find_element_by_class_name("author").text
+        # author = review.find_element_by_css_selector("a[data-hook='review-author'").text
+        author = review.find_element_by_class_name("author").text
         raw_date = review.find_element_by_class_name("review-date").text[
                    3:]  # 3: is to remove the "on " part of the date #TEST THIS TO SEE IF IN RANGE
         ordinal = datetime.strptime(raw_date, "%B %d, %Y").toordinal()
@@ -73,8 +74,9 @@ while (done==False & page_num <= 4):
         print("review count: " + str(review_count))
     if done: break
     next_button = driver.find_element_by_class_name("a-last") #.find_element_by_class_name("a-form-actions")
-    sleep(0.05 + random())
+    # sleep(0.05 + random())
     next_button.click()
+    sleep(0.3 + random())
     page_num += 1
 print("Done")
 print(df.shape)
